94cb9ecaeabb50c69aaff820d6fc3decfcae6a01
ItsMeNengYi <20040408gry@gmail.com>
2026-06-27 14:55:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 echo "Running your command"
-cobc -x LOAN.cob -o LOAN
-./LOAN
-if command -v py >/dev/null 2>&1; then
-  PYTHON_CMD=py
-elif command -v python3 >/dev/null 2>&1; then
-  PYTHON_CMD=python3
-else
-  PYTHON_CMD=python
-fi
-
-$PYTHON_CMD src/parser/parser.py
+streamlit run app.py --server.port=8501 --server.address=0.0.0.0
